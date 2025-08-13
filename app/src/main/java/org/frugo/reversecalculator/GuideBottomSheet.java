09b0719de977da2d0c3bca61894d9d3d4a0054fe
Diego Monti <diegmonti@users.noreply.github.com>
2025-08-13 15:08:54 +0200

    Format guide text with HTML

--- app/src/main/java/org/frugo/reversecalculator/GuideBottomSheet.java
+++ app/src/main/java/org/frugo/reversecalculator/GuideBottomSheet.java
@@ -7,6 +7,9 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.text.HtmlCompat;
+
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
@@ -15,6 +18,9 @@ public class GuideBottomSheet extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.guide_bottom_sheet, container, false);
+        View view = inflater.inflate(R.layout.guide_bottom_sheet, container, false);
+        TextView guideText = view.findViewById(R.id.guideText);
+        guideText.setText(HtmlCompat.fromHtml(getString(R.string.guide_text), HtmlCompat.FROM_HTML_MODE_LEGACY));
+        return view;
     }
 }
